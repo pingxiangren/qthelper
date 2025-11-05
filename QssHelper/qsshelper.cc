3c0acb0c0baafc693930b5e750fee3c77e96fe8c
lhh <honghui.lv@hoymiles.com>
2025-11-05 15:55:39 +0800

    fix: add QDebug include

--- QssHelper/qsshelper.cc
+++ QssHelper/qsshelper.cc
@@ -2,18 +2,21 @@
 
 #include <QFile>
 #include <QFont>
+#include <QDebug>
 
-bool LoadQssFile(QApplication *app, const QString &filePath) {
-    app->setFont(QFont("Microsoft Yahei", 9));  // 设置统一字体
+bool LoadQssFile(QApplication *app, const QString &filePath)
+{
+    app->setFont(QFont("Microsoft Yahei", 9)); // 设置统一字体
 
     QFile file(filePath);
     // cout << "path_qss:" << path_qss.toStdString() << endl;
-    if (!file.open(QFile::ReadOnly)) {
+    if (!file.open(QFile::ReadOnly))
+    {
         qDebug() << "打开Qss文件失败: " << filePath;
         return false;
     }
     QString styleSheet{QLatin1String(file.readAll())};
-    app->setStyleSheet(styleSheet);  // * 设置样式
+    app->setStyleSheet(styleSheet); // * 设置样式
 
     return true;
 }
