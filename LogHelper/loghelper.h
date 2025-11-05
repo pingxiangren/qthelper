a336a6727b066a1ac5cd14acfe87af5323f46d94
lhh <honghui.lv@hoymiles.com>
2025-11-05 16:16:27 +0800

    chore: remove comment

--- LogHelper/loghelper.h
+++ LogHelper/loghelper.h
@@ -4,8 +4,8 @@
 #include <QFile>
 
 /**
- * @brief 检查日志文件大小并执行轮转（重命名旧文件，创建新文件）。
- * @param file 要操作的QFile对象。
+ * @brief check and rotate log file
+ * @param file log file object
  */
 void checkAndRotateLogFile(QFile &file);
 
